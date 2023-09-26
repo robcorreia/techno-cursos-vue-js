8bde9bd7911482c8f88aaf1fbfdd642505969572
Robson Correia <robsoncorreia@192.168.100.4>
2023-09-26 11:44:55 -0300

    feat: css list

--- src/views/Cursos.vue
+++ src/views/Cursos.vue
@@ -8,7 +8,7 @@
         <div>
           <h1>{{ data.titulo }}</h1>
           <p>{{ data.descricao }}</p>
-          <ul>
+          <ul class="cursos-lista">
             <li v-for="curso in data.cursos" :key="curso.id">
               <h2>
                 <router-link :to="{ name: 'curso', params: { curso: curso.id } }">{{ curso.nome }} - {{ curso.totalAulas
@@ -34,4 +34,8 @@ export default {
   }
 }
 </script>
-<style></style>
+<style>
+.cursos-lista li {
+  margin-bottom: 40px;
+}
+</style>
